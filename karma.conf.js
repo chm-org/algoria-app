9b9c2f3518afcec66998f0a36a26bbbe3032adef
romanKrds <roman.kordas7@gmail.com>
2024-06-13 18:48:16 +0300

    fix build configuration

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/challenge'),
+      dir: require('path').join(__dirname, './coverage/algoria'),
       subdir: '.',
       reporters: [
         { type: 'html' },
