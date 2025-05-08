08ad84c21a5c04043d1efdf72f6aedf998409d1b
romanKrds <roman.kordas7@gmail.com>
2025-05-08 12:41:21 +0300

    add primeng

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,25 @@
 import { HttpClient, provideHttpClient } from "@angular/common/http";
 import { ApplicationConfig, importProvidersFrom, provideExperimentalZonelessChangeDetection } from '@angular/core';
 import { provideAnimations } from "@angular/platform-browser/animations";
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideRouter } from '@angular/router';
 import { TranslateLoader, TranslateModule } from "@ngx-translate/core";
 import { TranslateHttpLoader } from "@ngx-translate/http-loader";
 import { MonacoEditorModule } from "ngx-monaco-editor-v2";
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeng/themes/aura';
 
 import { routes } from './app.routes';
 
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideAnimationsAsync(),
+    providePrimeNG({
+        theme: {
+            preset: Aura
+        }
+    }),
     provideExperimentalZonelessChangeDetection(),
     provideRouter(routes),
     provideAnimations(),
