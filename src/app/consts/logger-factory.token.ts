b161f15acaeff44fcfb9b683606d2736c31caaf0
romanKrds <roman.kordas7@gmail.com>
2025-12-15 10:06:14 +0200

    use prod dependencies by default

--- src/app/consts/logger-factory.token.ts
+++ src/app/consts/logger-factory.token.ts
@@ -1,4 +1,4 @@
 import { InjectionToken } from '@angular/core';
-import { LoggerFactory } from '../../../../algoria-utils';
+import { LoggerFactory } from 'algoria-utils';
 
 export const LOGGER_FACTORY = new InjectionToken<LoggerFactory>('LOGGER_FACTORY');
--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3001/algoria-challenges'
+  // uncomment to use self hosted challenges
+  // apiUrl: 'http://localhost:3001/algoria-challenges',
+  apiUrl: 'https://chm-org.github.io/algoria-challenges'
 };
